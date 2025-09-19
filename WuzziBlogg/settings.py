8d99e586fe0ad6f4c500f5c56714e9e60962857a
Sampson Ikechukwu Ewuzie <sampson.ewuzie@abujaelectricity.com>
2025-09-19 19:19:19 +0100

    updated the yaml file

--- WuzziBlogg/settings.py
+++ WuzziBlogg/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'your-render-app-name.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'RENDER_EXTERNAL_HOSTNAME']
 
 
 # Application definition
