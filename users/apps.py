b24c158747868595fcf2d4517d1a9bd895b51148
Sampson Ikechukwu Ewuzie <sampson.ewuzie@abujaelectricity.com>
2025-09-19 21:30:52 +0100

    updated yaml file

--- users/apps.py
+++ users/apps.py
@@ -1,26 +1,5 @@
 from django.apps import AppConfig
-from django.contrib.auth import get_user_model
-from django.db.models.signals import post_migrate
-import os
-
-
-def create_superuser(sender, **kwargs):
-    if os.environ.get("CREATE_SUPERUSER", "False") == "True":
-        User = get_user_model()
-        username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
-        email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
-        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "adminpass")
-
-        if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(username=username, email=email, password=password)
-            print(f"✅ Superuser '{username}' created!")
-        else:
-            print(f"ℹ️ Superuser '{username}' already exists. Skipping creation.")
-
 
 class UsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "users"
-
-    def ready(self):
-        post_migrate.connect(create_superuser, sender=self)
